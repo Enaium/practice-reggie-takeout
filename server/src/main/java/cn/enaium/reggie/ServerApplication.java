8eff21910c47e36ae643a32176fb57b27d37e92e
Enaium <wyy-666@hotmail.com>
2023-01-09 20:58:59 +0800

    server

--- server/src/main/java/cn/enaium/reggie/ServerApplication.java
+++ server/src/main/java/cn/enaium/reggie/ServerApplication.java
@@ -1,13 +1,16 @@
 package cn.enaium.reggie;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+        log.info("项目启动成功...");
+    }
 
 }
